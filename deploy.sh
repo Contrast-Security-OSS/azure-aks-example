97b282a88bb15c2009e92471bddd5a2f6e8f65ef
admiralappsec <80486690+admiralappsec@users.noreply.github.com>
2021-05-27 13:26:19 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-az webapp deployment source config-zip --resource-group $resourcegroupname --name $webappname --src ./deploy/deploy.zip
-az webapp restart --name $webappname --resource-group $resourcegroupname
-
-echo "Deploy complete."
